25b4522532ddf2cdcaefe3800df2dc445e9612ab
adinsaw <dinesh.amancharla@Namratha>
2016-02-04 11:48:20 -0600

    added the binary search class.

--- searchandsort/src/searchandSort/BinarySearch.java
+++ searchandsort/src/searchandSort/BinarySearch.java
@@ -1,12 +1,38 @@
 package searchandSort;
 
+import java.util.Arrays;
+
 public class BinarySearch {
 	
+static int[] data={4,7,9,10,67,78};
+static int input=2;
+
 	public static void main (String[] args)
 	{
-		System.out.println("TEST");
-		System.out.println("TEST");
-		System.out.println("TEST");
+		
+		int value=searcharray(data,0,data.length,input);	
+		System.out.println(value);
+		
+	}
+	
+	public static int searcharray(int[] A,int loIndex,int hiIndex ,int B )
+	{
+		
+		   if (loIndex > hiIndex) {
+			   return -1;
+		   }
+		   else 
+			   {
+			   int middle = (int) (loIndex+hiIndex)/2;
+			   if(A[middle]==B ){return middle;}	   
+			   else if(A[middle]>B)
+		{			return searcharray(A,loIndex,middle-1,B);
+	}else return searcharray(A,middle+1,hiIndex,B);
+	}
+		
+		
+		
+		
 	}
 
 }
